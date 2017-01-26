2eca631b5ac9c5cc74913daa3ede56d6985493f0
Hakim Hauston <hakim.hauston@gmail.com>
2017-01-26 18:28:51 +0800

    accepting token [wip]

---
+++ app/controllers/apns/CasinoApnsController.java
@@ -0,0 +1,43 @@
+package controllers.apns;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.notnoop.apns.APNS;
+import com.notnoop.apns.ApnsService;
+
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import views.html.*;
+
+public class CasinoApnsController extends Controller {	
+	
+	private static final String certPath = 
+			"conf/certificates/apns_dev_export_single_selection_root_from_keychain.p12";
+	private static final String deviceToken = 
+			"2404d486d44d6718407ef5db94639b9a99942de0bdbc879ea4c5602625e65e1b";
+	
+	public static Result testPushAction() {
+		ApnsService service =
+			    APNS.newService()
+			    .withCert(certPath, "123456")
+			    .withSandboxDestination()
+			    .build();
+		String payload = APNS.newPayload().badge(1).alertBody("sent using notnoop play entry point from testpush!").build();
+		service.push(deviceToken, payload);
+		return ok(index.render());
+	}
+	
+	public Result registrationAction() {
+		Map<String, String[]> postData = request().body().asFormUrlEncoded();
+		for ( Map.Entry<String, String[]> entry : postData.entrySet() ) {
+			Logger.error("registration pair: " + entry.getKey() + " : " + entry.getValue().toString());
+		}
+		return ok(index.render());
+	}
+
+}
